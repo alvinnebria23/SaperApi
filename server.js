f99fd427011d4a550b28d2b71a80bb7a95367972
alvinnebria23 <nebriaalvin23@gmail.com>
2023-06-13 22:33:28 +0800

    Added https create server function

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import UserRouter from "./routes/UserRouter.js"
 import ShopeeRouter from "./routes/ShopeeRouter.js";
 import ProxyUserRouter from "./routes/ProxyUserRouter.js";
 import LinkRouter from "./routes/LinkRouter.js";
+import https from "https";
+import fs from "fs";
 import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 8081;
@@ -17,10 +19,14 @@ app.use("/api/v1/shopee", ShopeeRouter);
 app.use("/api/v1/link", LinkRouter);
 app.use("/user", ProxyUserRouter)
 
-// app.use("/test", () => {
-//   console.log('imhere');
-// });
+// Load SSL certificate and private key 
+const options = { 
+	key: fs.readFileSync('/path/to/private_key.pem'), 
+	cert: fs.readFileSync('/path/to/certificate.pem') 
+};
+// Create HTTPS server 
+const server = https.createServer(options, app);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
