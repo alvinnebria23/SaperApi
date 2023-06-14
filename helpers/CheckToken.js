4ee8a5ac0289e90816222eababbe7e5ffd1af5a9
alvinnebria23 <nebriaalvin23@gmail.com>
2023-06-14 23:16:48 +0800

    Modified the codes for checktoken

--- helpers/CheckToken.js
+++ helpers/CheckToken.js
@@ -1,21 +1,23 @@
 import jwt from "jsonwebtoken";
 import { INVALID, UNAUTHORIZED } from "../constants/HttpCodes.js";
+import logger from "../loggers/logger.js";
 
 const checkToken = (req, res, next) => {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
-  if (token == null) return res.sendStatus(UNAUTHORIZED);
+  try {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token == null) return res.sendStatus(UNAUTHORIZED);
 
-  jwt.verify(token, process.env.API_KEY + process.env.API_SECRET, (error) => {
-    if(error){
-      if(error.name === 'TokenExpiredError'){
-        res.sendStatus(UNAUTHORIZED);
-      }else {
-        res.sendStatus(INVALID);
-      }
-    };
-    next();
-  });
+    jwt.verify(token, process.env.API_KEY + process.env.API_SECRET, (error) => {
+      if(error){
+        logger.error(`ERROR_NAME=${error.name} APP_ID=${req.body.appId}`);
+        return res.sendStatus(UNAUTHORIZED);
+      };
+      next();
+    });
+  } catch (error) {
+    logger.error("ERROR MESSAGE: " + error.message);
+  }
 };
 
 export default checkToken;
